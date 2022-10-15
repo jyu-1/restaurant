bac6beec180950ca2af7ab27fb79f5c13f86db87
jia <jiabids@gmail.com>
2022-10-14 17:23:21 -0700

    link buttons to navigate pages

--- src/index.js
+++ src/index.js
@@ -16,6 +16,16 @@ export const createPage = (targetEle, type, className) => {
 
 const mainLayout = (() => {
     const header = createPage(mainDiv, "div", "header");
+    const nav = createPage(mainDiv, "div", "nav");
+    const button1 = createPage(nav.ele, "button", "nav-button");
+    const button2 = createPage(nav.ele, "button", "nav-button");
+    const button3 = createPage(nav.ele, "button", "nav-button");
+    button1.changeText("Home");
+
+    button2.changeText("Menu");
+
+    button3.changeText("Contact");
+
     const container = createPage(mainDiv, "div", "container");
     const footer = createPage(mainDiv, "div", "footer");
     const mainContainer = createPage(container.ele, "div", "main");
@@ -23,7 +33,31 @@ const mainLayout = (() => {
     header.changeText("LA's Best Steak");
     footer.changeText("Copyright © 2022 jyu-1");
 
-    //mainPage(mainContainer);
-    menuPage(mainContainer);
-    //contactPage(mainContainer);
+    mainPage(mainContainer);
+    button1.ele.classList.add("current-nav");
+
+    button1.ele.addEventListener("click", () => {
+        button1.ele.classList.add("current-nav");
+        button2.ele.classList.remove("current-nav");
+        button3.ele.classList.remove("current-nav");
+        mainContainer.ele.innerText = "";
+        mainPage(mainContainer);
+    })
+
+    button2.ele.addEventListener("click", () => {
+        button1.ele.classList.remove("current-nav");
+        button2.ele.classList.add("current-nav");
+        button3.ele.classList.remove("current-nav");
+        mainContainer.ele.innerText = "";
+        menuPage(mainContainer);
+    })
+
+    button3.ele.addEventListener("click", () => {
+        button1.ele.classList.remove("current-nav");
+        button2.ele.classList.remove("current-nav");
+        button3.ele.classList.add("current-nav");
+        mainContainer.ele.innerText = "";
+        contactPage(mainContainer);
+    })
 })();
+
